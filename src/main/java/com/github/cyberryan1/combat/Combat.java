f1d561a1b57987dd687587a97e76e5b53e28f940
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-01-01 01:45:26 -0600

    Initial commit

---
+++ src/main/java/com/github/cyberryan1/combat/Combat.java
@@ -0,0 +1,12 @@
+package com.github.cyberryan1.combat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Combat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
